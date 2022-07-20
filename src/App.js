08638d11a3db62806f6fdd255e87869ec228f06f
Thulasiram111 <109522385+Thulasiram111@users.noreply.github.com>
2022-07-20 19:14:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        hello world
+        this is thulasiram learning AWS
       </header>
     </div>
   );
